69ab6ef7b6a7dfc91e6639144d4bddb8a0431c5d
hyperchessbot <hyperchessbot@gmail.com>
2020-12-25 16:55:55 +0000

    Find proper username

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ const puzzles = fs.readFileSync("leaderboard.csv").toString().split("\n")
 app.get('/', (req, res) => {
 	let username = req.query.getpuzzles
 	if(username){
-		const matcher = new RegExp(`([0-9]+),${username},([0-9]+),(.*)`, "i")
+		const matcher = new RegExp(`([0-9]+),(${username}),([0-9]+),(.*)`, "i")
 		const ups = puzzles.find(line => line.match(matcher))
 		if(ups){
 			console.log(ups)
 			const m = ups.match(matcher)
 			const rank = m[1]
-			const num = m[2]
-			const puzzleIds = m[3].split(" ")
+			username = m[2]
+			const num = m[3]
+			const puzzleIds = m[4].split(" ")
 			const puzzleUrls = puzzleIds.map(puzzleId => `<a target="_blank" rel="noopener noreferrer" href="https://lichess.org/training/${puzzleId}">${puzzleId}</a>`).join("<br>")
 
 			res.send(`wow , <b style="color:#070">${username}</b> has games in puzzles , rank <b style="color:#00f">${rank}</b> , number of puzzles <b style="color:#707">${num}</b><hr>${puzzleUrls}`)	
